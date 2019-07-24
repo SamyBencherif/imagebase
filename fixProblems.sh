877c58707c2513259d745a0d48d11aa21780009c
Samy Bencherif <samybencherif@live.com>
2019-07-23 22:48:27 -0400

    .

--- fixProblems.sh
+++ fixProblems.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-sudo curl -L "https://github.com/SamyBencherif/imagebase/blob/master/ffmpeg?raw=true" -o /usr/local/bin/ffmpeg
+#sudo curl -L "https://github.com/SamyBencherif/imagebase/blob/master/ffmpeg?raw=true" -o /usr/local/bin/ffmpeg
 
-sudo curl -L "https://github.com/SamyBencherif/imagebase/blob/master/ffprobe?raw=true" -o /usr/local/bin/ffprobe
+#sudo curl -L "https://github.com/SamyBencherif/imagebase/blob/master/ffprobe?raw=true" -o /usr/local/bin/ffprobe
+
+sudo chmod +x /usr/local/bin/ffmpeg
+sudo chmod +x /usr/local/bin/ffprobe
+
+cd ~/Desktop
+
+youtube-dl --extract-audio --audio-format mp3 "https://www.youtube.com/watch?v=Ao5Axxh0Oh8"
+
 
