d53b9945ee1db1c229ed36af5cff642a296069a0
Samy Bencherif <samybencherif@live.com>
2019-07-23 22:32:24 -0400

    .

---
+++ fixProblems.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo curl -L "https://github.com/SamyBencherif/imagebase/blob/master/ffmpeg?raw=true" -o /usr/local/bin/ffmpeg
+
+sudo curl -L "https://github.com/SamyBencherif/imagebase/blob/master/ffprobe?raw=true" -o /usr/local/bin/ffprobe
+
